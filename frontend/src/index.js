f8666a9ae6af4954e9728a792efd0a4b89a99aaf
Samuel Solomon <samuelsolomon452@gmail.com>
2024-11-08 16:50:05 +0300

    folder stracture change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import ShopContextProvider from './Context/Context';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider>
